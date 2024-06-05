38a3f1272137520cea0c689f233f0f37d4c7a83d
Koharu Saeki <koharu0929alexandros@gmail.com>
2024-06-05 21:06:01 +0900

    メトロノームと時間のずれを直した！

--- metronome-app/CalenderTabViewController.swift
+++ metronome-app/CalenderTabViewController.swift
@@ -6,24 +6,59 @@
 //
 
 import UIKit
+import EventKit
 
-class CalenderTabViewController: UIViewController {
+class CalenderTabViewController: UIViewController, UICalendarViewDelegate {
+    var calendarView: UICalendarView!
+    var bpmLabel: UILabel!
+    
+    var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
 
     override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpCalendar()
+        setupLabel()
         
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUpCalendar(){
+        calendarView = UICalendarView()
+        calendarView.delegate = self
+        calendarView.tintColor = UIColor(red: 229/255, green: 145/255, blue: 239/255, alpha: 1)
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(calendarView)
+        
+        NSLayoutConstraint.activate([
+            self.view.topAnchor.constraint(equalTo: calendarView.topAnchor),
+            self.view.leftAnchor.constraint(equalTo: calendarView.leftAnchor),
+            self.view.rightAnchor.constraint(equalTo: calendarView.rightAnchor)
+        ])
     }
-    */
-
+    
+    func setupLabel() {
+        bpmLabel = UILabel()
+        bpmLabel.text = "あああああ"
+        bpmLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bpmLabel)
+        bpmLabel.textAlignment = .center
+        NSLayoutConstraint.activate([
+            bpmLabel.topAnchor.constraint(equalTo: calendarView.bottomAnchor, constant: 20),
+            bpmLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            bpmLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
+            calendarView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100)
+        ])
+    }
+    // ユーザーが日付を選択したときの処理
+    func touchCalendarView(_ calendarView: UICalendarView, didSelectDate dateComponents: DateComponents) {
+        guard let date = Calendar.current.date(from: dateComponents) else { return }
+        let key = dateFormatter.string(from: date)
+        let maxBpm = UserDefaults.standard.double(forKey: key)
+        bpmLabel.text = "Max BPM on \(key): \(maxBpm)"
+        print("key: ", key)
+        print("maxBpm :", maxBpm)
+    }
 }
--- metronome-app/PracticeTabViewController.swift
+++ metronome-app/PracticeTabViewController.swift
@@ -17,6 +17,8 @@ class PracticeTabViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var changeTextField: UITextField! // 一回ごとに変えるbpm
     @IBOutlet weak var messageLabel: UILabel! // 残り時間表示
     
+    @IBOutlet var highestLabel: UILabel! // 後で消す、highestを記録
+    
     @IBOutlet var togglePracticeButton: UIButton!
     
     @IBOutlet var progressView: UIProgressView!
@@ -77,6 +79,7 @@ class PracticeTabViewController: UIViewController, UITextFieldDelegate {
         self.view.endEditing(true)
     }
     
+    // 振動有無
     @IBAction func toggleVibrationSwitch(_ sender: UISwitch) {
         isVibrationEnabled = sender.isOn  // スイッチの状態に基づいて振動を設定
     }
@@ -101,6 +104,7 @@ class PracticeTabViewController: UIViewController, UITextFieldDelegate {
         durationTimer?.invalidate()
         isMetronomeActive = false
         saveLastBpm()
+        saveBpmForCurrentSession(bpm: currentBpm)
         print("Metronome stopped")
     }
     
@@ -117,8 +121,10 @@ class PracticeTabViewController: UIViewController, UITextFieldDelegate {
             messageLabel.text = "前回の記録は: \(Int(bpm)) だよ！"
         }
     }
-    
-    func saveLastBpm(){ // 最後のbpmを保存
+
+
+    // 最後に練習したbpmを保存
+    func saveLastBpm(){
         let defaults = UserDefaults.standard
         let lastSessionData = [
             "bpm": currentBpm,
@@ -126,6 +132,22 @@ class PracticeTabViewController: UIViewController, UITextFieldDelegate {
         ] as [String : Any]
         defaults.set(lastSessionData, forKey: "LastSession")
         defaults.synchronize()
+    }
+    
+    // 1日の最高Bpmを保存したい！！！！！！
+    func saveBpmForCurrentSession(bpm: Double) {
+        let key = getCurrentDateKey()
+        var maxBpm = UserDefaults.standard.double(forKey: key)
+        if bpm > maxBpm {
+            UserDefaults.standard.set(bpm, forKey: key)
+        }
+        highestLabel.text = "今日の最高bpmは \(Int(bpm))"
+    }
+    
+    func getCurrentDateKey() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        return dateFormatter.string(from: Date())
     }
     
     func stopPractice(){
@@ -151,7 +173,6 @@ class PracticeTabViewController: UIViewController, UITextFieldDelegate {
     
     func updateProgressView(){
         guard let durationText = durationTextField.text, let totalDuration = Float(durationText) else {
-            print("Invalid duration input")
             return
         }
         
@@ -164,12 +185,20 @@ class PracticeTabViewController: UIViewController, UITextFieldDelegate {
         print("progress :", progress)
     }
     
+    func resetProgressView() {
+        // プログレスバーをすぐにフルに戻す
+        progressView.setProgress(1.0, animated: false)
+    }
+    
     func updateMessageLabel(){
         displayTextForSeconds("tempo is up!", duration: 3)
     }
     
+    var i = 1
     
     func startMetronomeWithTimer() {
+        print(i, "回目のstartWithTimer")
+        i+=1
         durationTimer?.invalidate()
         metronomeLabel.text = String(format: "%.0f", currentBpm)
         durationTimer = Timer.scheduledTimer(timeInterval: 60.0 / currentBpm, target: self, selector: #selector(playSound), userInfo: nil, repeats: true)
@@ -207,8 +236,6 @@ class PracticeTabViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func togglePractice(_ sender: UIButton){
         togglePracticeButton.isEnabled = true;
-        
-        print(isMetronomeActive)
         if(isMetronomeActive){
             stopPractice()
         }else{
@@ -278,8 +305,7 @@ class PracticeTabViewController: UIViewController, UITextFieldDelegate {
         self.bpmIncrement = bpmIncrement
         self.currentBpm = initialBpm
         self.currentTime = durationInSeconds
-        self.updateProgressView()
-        startMetronomeWithTimer()
+        self.startMetronomeWithTimer()
         
         countdownTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] timer in
             guard let self = self else {return}
@@ -289,15 +315,14 @@ class PracticeTabViewController: UIViewController, UITextFieldDelegate {
             self.currentTime -= 1.0
             self.updateProgressView()
             
-            
             if self.currentTime <= 0{
-                self.updateProgressView()
+                //self.updateProgressView()
+                self.resetProgressView()
                 self.currentTime = durationInSeconds
                 
-                self.updateMessageLabel()
-                self.updateProgressView()
-                self.startMetronomeWithTimer()
                 self.currentBpm += bpmIncrement
+                self.startMetronomeWithTimer()
+                self.updateMessageLabel()
                 
             }
         }
